7519ce5dbe6638c72ba7e87a21720558a1a48439
zhangzeyan <zhangzeyan@zhixueyun.com>
2018-03-23 00:56:18 +0800

    chapter2 for type of data

---
+++ app/src/main/java/com/simple/kotlin/chapter2/TypeOfData.kt
@@ -0,0 +1,40 @@
+package com.simple.kotlin.chapter2
+
+/**
+ * @fileName:
+ * @author:          zhangzeyan
+ * @date:            2018/3/23
+ * @time:            上午12:36
+ * @description:
+ **/
+
+var aBoolean: Boolean = true
+
+var anInt: Int = 8
+var maxInt: Int = Int.MAX_VALUE
+var minInt: Int = Int.MIN_VALUE
+
+var aLong: Long = 123131131324134324L
+var maxLong: Long = Long.MAX_VALUE
+var minLong: Long = Long.MIN_VALUE
+
+var aFloat: Float = 12.9f
+var maxFloat: Float = Float.MAX_VALUE
+var minFloat: Float = Float.MIN_VALUE
+
+
+fun main(args: Array<String>) {
+
+    println(aBoolean)
+    println(anInt)
+    println(maxInt)
+    println(minInt)
+
+    println(aLong)
+    println(maxLong)
+    println(minLong)
+
+    println(aFloat)
+    println(maxFloat)
+    println(minFloat)
+}
