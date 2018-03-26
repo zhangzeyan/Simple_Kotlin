df57ab1df7828cd38c3cf3463939fa114a83d262
zhangzeyan <zhangzeyan@zhixueyun.com>
2018-03-27 00:16:35 +0800

    interface demo

--- app/src/main/java/com/simple/kotlin/chapter4/InterfaceDemo.kt
+++ app/src/main/java/com/simple/kotlin/chapter4/InterfaceDemo.kt
@@ -1,27 +1,47 @@
 package com.simple.kotlin.chapter4
 
 
-fun interfaceDemo(){
-    var c = C("Simple")
-    println(c.aMethod())
-    println(c.bMethod())
-    println(c.myTest())
+fun interfaceDemo() {
+//    var c = C("Simple")
+//    println(c.aMethod())
+//    println(c.bMethod())
+//    println(c.myTest())
+
+//    val h1 = H1()
+//    val h2 = H2()
+//    val h = H(h1, h2)
+//    h.fMethod()
+//    h.fMethod1()
+
+//    println(G3(99).x())
+//    println(G3(100).x())
+//    println(G3(200).x())
+//    println(G3(-1).x())
+
+//    J.jMethod()
+
+
+    println(K.ofDouble(12.8).myTest())
+    println(K.myStaticMethod())
+
 }
 
-abstract class B{
+//抽象类和接口
+abstract class B {
     abstract fun bMethod()
 }
 
-interface A{
-    var a:String
-    fun myTest(){
+interface A {
+    var a: String
+    fun myTest() {
         println(a)
     }
+
     fun aMethod()
 }
 
 
-class C(override var a: String) : B(),A{
+class C(override var a: String) : B(), A {
 
     override fun aMethod() {
         println("a method $a")
@@ -32,4 +52,91 @@ class C(override var a: String) : B(),A{
 
     }
 
+}
+
+//属性复写
+
+open class D(open val b: String, a: Int)
+
+class E(override val b: String, a: Int) : D(b, a)
+
+
+//接口代理
+interface F {
+    fun fMethod()
+}
+
+interface F1 {
+    fun fMethod1()
+}
+
+
+class H(val fMethod: F, val fMethod1: F1) : F by fMethod, F1 by fMethod1
+
+class H1 : F {
+    override fun fMethod() {
+        println("H1 run fMethod")
+    }
+}
+
+class H2 : F1 {
+    override fun fMethod1() {
+        println("H2 run fMethod1")
+    }
+}
+
+//复写父类接口方法
+abstract class G {
+    open fun x(): Int = 11
+}
+
+interface G1 {
+    fun x(): Int = 12
+}
+
+interface G2 {
+    fun x(): Int = 13
+}
+
+class G3(val y: Int) : G(), G1, G2 {
+    override fun x(): Int {
+        return when (y) {
+            in 0 until 100 -> super<G>.x()
+            in 100 until 200 -> super<G1>.x()
+            in 200..Int.MAX_VALUE -> super<G2>.x()
+            else -> y
+        }
+    }
+}
+
+//单例 object
+object J {
+    fun jMethod() {
+        println("simple")
+    }
+}
+
+//伴生对象
+class K  private constructor(val value: Double) {
+    companion object {
+        fun ofDouble(double: Double):K{
+            return K(double)
+        }
+
+        @JvmStatic
+        fun myStaticMethod():Int{
+            return 12
+        }
+
+        fun myStaticMethod1():Int{
+            return 13
+        }
+
+        @JvmField
+        val tag = 12
+    }
+
+    fun myTest(): String{
+        return "the number is $value"
+    }
 }
---
+++ app/src/main/java/com/simple/kotlin/chapter4/InterfaceJava.java
@@ -0,0 +1,18 @@
+package com.simple.kotlin.chapter4;
+
+/**
+ * @fileName:
+ * @author: zhangzeyan
+ * @date: 2018/3/27
+ * @time: 上午12:06
+ * @description:
+ **/
+public class InterfaceJava {
+    public static void main(String[] args){
+        K.myStaticMethod();
+        K.Companion.myStaticMethod1();
+        if(K.tag ==19){
+
+        }
+    }
+}
